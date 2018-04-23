c1ab72e6f990509ec07c380ab076a463d60d1115
Baek-Hwan-Gi <abcy100@nate.com>
2018-04-23 19:51:05 +0900

    TEST
    전에 만들어둔 맵 스크롤
    + 총알 딜레이 없이쏘는것

---
+++ NewTeamKit/Assets/2DGamekit/Scripts/MoveTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MoveTest : MonoBehaviour {
+
+    private Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("space"))
+        {
+            rb.AddForce(Vector3.up * 100.0f);
+        }
+	}
+}
