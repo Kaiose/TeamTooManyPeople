086d019071a18a3aa8e286f0078f294c3d6fb815
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2018-04-25 00:10:27 +0900

    Add files via upload

---
+++ Scrolling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+	public float Scroll_Speed = 0.3f;
+	private Renderer rd;
+	private float Offset;
+
+
+	// Use this for initialization
+	void Start () {
+		rd = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Offset += Time.deltaTime * Scroll_Speed;
+		rd.material.mainTextureOffset = new Vector2(Offset, 0);
+	}
+}
---
+++ Shooting_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+			transform.Translate(new Vector2(0, 0.1f));
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate(new Vector2(0, -0.1f));
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate(new Vector2(0.1f, 0));
+		if (Input.GetKey(KeyCode.A))
+			transform.Translate(new Vector2(-0.1f, 0));
+
+	}
+}
