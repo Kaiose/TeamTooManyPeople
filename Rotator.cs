4b9291d331871fb1dcf0b6ea7161a62ebf71ca66
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-05-08 03:55:16 +0900

    에너미 회전 코드

---
+++ Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rotator : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 10.0f;
+
+	Quaternion origin;
+
+	Coroutine flipper = null;
+	// Use this for initialization
+	void Start () {
+		origin = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+		transform.Rotate (Vector3.back* speed);
+	}
+
+}
