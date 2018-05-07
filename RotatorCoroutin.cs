c83a7e9dc0cab2edcdd06daf98629924185f4c15
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-05-08 03:55:56 +0900

    에너미 회전 코루틴사용 코드

---
+++ RotatorCoroutin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rotator : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 40.0f;
+
+	Quaternion origin;
+
+	Coroutine flipper = null;
+	// Use this for initialization
+	void Start () {
+		origin = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+		transform.Rotate (Vector3.back* speed);
+	//	StartCoroutine (flip ());
+	}
+	//IEnumerator flip()
+	//{
+	//	transform.rotation = origin;
+	//	int n_frames = 100;
+	//	while (n_frames-- > 0) {
+	//		transform.Rotate (Vector3.forward * speed);
+	//		yield return null;
+	//	}
+	//}
+}
