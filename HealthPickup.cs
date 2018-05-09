5a1ce23895a7f2ab39eb3b3e3aeba219b1ac77bf
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2018-05-09 20:38:58 +0900

    Add files via upload

---
+++ HealthPickup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gamekit2D
+{
+	public class HealthPickup : MonoBehaviour
+	{
+		public enum ItemAttribute { Health, Time_recovery };
+
+		public ItemAttribute MineAttribute;
+		public int healthAmount = 1;
+		public UnityEvent OnGivingHealth;
+
+		private Shooting_move shoot_move;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject == PlayerCharacter.PlayerInstance.gameObject)
+			{
+				switch (MineAttribute)
+				{
+					case ItemAttribute.Health:
+						Damageable damageable = PlayerCharacter.PlayerInstance.damageable;
+						if (damageable.CurrentHealth < damageable.startingHealth)
+						{
+							damageable.GainHealth(Mathf.Min(healthAmount, damageable.startingHealth - damageable.CurrentHealth));
+							OnGivingHealth.Invoke();
+						}
+						break;
+					case ItemAttribute.Time_recovery:
+						shoot_move = other.gameObject.GetComponent<Shooting_move>();
+
+						if (shoot_move.rest_time < 900)
+							shoot_move.rest_time = shoot_move.rest_time + 100;
+						else
+							shoot_move.rest_time = 1000f;
+
+						Destroy(this.gameObject);
+						
+						break;
+				}
+			}
+		}
+	}
+}
+
+
+
--- Shooting_move.cs
+++ Shooting_move.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Shooting_move : MonoBehaviour {
+public class Shooting_move : MonoBehaviour
+{
 
 	public float Move_Speed = 0.1f;
 
+	public float rest_time = 1000f;
+
+	public Slider time_slider;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+
+		// if (rest_time > 0){
 
 		if (Input.GetKey(KeyCode.W))
 			transform.Translate(new Vector2(0, Move_Speed));
@@ -26,5 +36,22 @@ public class Shooting_move : MonoBehaviour {
 		if (Input.GetKey(KeyCode.A))
 			transform.Translate(new Vector2(-Move_Speed, 0));
 
+		/*}
+	else
+	{
+		transform.Translate(new Vector2(0, -Move_Speed));
+	}*/
+	}
+
+	private void FixedUpdate()
+	{
+		Time_tiking();
+	}
+
+	public void Time_tiking()
+	{
+		if (rest_time > 0)
+			rest_time = rest_time - 0.5f;
+		time_slider.value = rest_time;
 	}
 }
