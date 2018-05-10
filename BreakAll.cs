80b85761ec6a3666a2658ca66065f57b23899e70
Kimkinam <kaiose30@gmail.com>
2018-05-11 02:15:37 +0900

    lightning
    asd

---
+++ BreakAll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+public class BreakAll : MonoBehaviour {
+
+    public float Speed = 15.0f;
+    private float distance = 0.0f;
+    public GameObject go;
+    // Use this for initialization
+	void Start () {
+        Speed = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3 (transform.position.x + Speed * Time.deltaTime, transform.position.y, 0);
+        if (transform.position.x > go.transform.position.x) gameObject.SetActive(false);
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        EnemyBehaviour enemy = coll.gameObject.GetComponent<EnemyBehaviour>();
+        enemy.Die2();
+    }
+
+
+}
---
+++ UseLightning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseLightning : MonoBehaviour {
+
+    public GameObject go;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckKey();
+
+    }
+
+
+    void CheckKey()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            go.SetActive(true);
+        }
+    }
+
+}
